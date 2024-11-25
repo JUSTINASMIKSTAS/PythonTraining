59c23bcf0de9e851defece2aa60865b4dbf62f58
JUSTINASMIKSTAS <satskim@gmail.com>
2024-11-25 14:11:25 +0200

    first_lesson

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+from pprint import pprint as print
+# my_list= [1,2]
+#
+# result = 6 in my_list # patikrinti ar yra liste
+# for elem in enumerate(my_list):
+#     print(my_list)
+
+    # d2_list = []
+
+#     for _ in range(dim):
+#         new_list = []
+#         for _ in range(dim):
+#             d2_list.append(colum,symbol)
+# print(d2_list)
+
+
+dim = 10
+symbol = '*'
+
+my_2d_list =  [['*']*dim]*dim
+d2 = [[0,0,0],[1,1,1],[2,2,2]]
+print(my_2d_list)
